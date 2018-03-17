9de21484977a8b6b8c9645843dac737aa56576e5
Dave <37315071+DRAPP1@users.noreply.github.com>
2018-03-17 17:23:05 -0500

    Test Expressions

--- src/TestExpressions.java
+++ src/TestExpressions.java
@@ -6,14 +6,14 @@
 
 /*
  	Your predicted values for the expressions
-	1.
-	2.
-	3.
-	4.
-	5.
-	6.
-	7.
-	8.
+	1. 8
+	2. 11
+	3. 12
+	4. no clue
+	5. no clue
+	6. 11
+	7. 11
+	8. 11
  
  	After testing the expressions in the main method, indicate above if your predictions were correct.
 */
@@ -21,7 +21,15 @@
 public class TestExpressions {
 
 	public static void main(String[] args) {
-		System.out.println("YOUR_TEST_EXPRESSION");
+		System.out.println(2 + 3 * 4 - 6);
+		System.out.println(14 / 7 * 2 + 30 / 5 + 1);
+		System.out.println((12 + 6) / 3 * 2);
+		System.out.println((238 % 10 + 3) % 7);
+		System.out.println(6 * 7 % 4);
+		System.out.println(2 + 2 + 3 + 4);
+		System.out.println("2 + 2" + 3 + 4);
+		System.out.println(3 + 4 + "2 + 2");
+		
 	}
 
 }
