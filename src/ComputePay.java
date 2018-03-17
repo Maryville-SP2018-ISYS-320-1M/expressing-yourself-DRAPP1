acba49034cc342d235fdeed06777ab3cdb09766c
Dave <37315071+DRAPP1@users.noreply.github.com>
2018-03-17 17:21:07 -0500

    Compute Pay

--- src/ComputePay.java
+++ src/ComputePay.java
@@ -6,13 +6,18 @@
 
 public class ComputePay {
 	public static void main(String[] args) {
+		int totalHours= 4+5+8+4;
+		double salary= 8.75;
+		double totalPay= totalHours* salary;
+		
 		System.out.println("My total hours worked:");
-		System.out.println(4 + 5 + 8 + 4);
+		System.out.println(totalHours);
+		System.out.println();
+	    System.out.println("My hourly salary:");
+	    System.out.println(salary);
+	    System.out.println();
+	    System.out.println("My total pay:");
+	    System.out.println(totalPay);
 		
-		System.out.println("My hourly salary:");
-		System.out.println("$8.75");
-		
-		System.out.println("My total pay:");
-		System.out.println((4 + 5 + 8 + 4) * 8.75);
 	}
 }
