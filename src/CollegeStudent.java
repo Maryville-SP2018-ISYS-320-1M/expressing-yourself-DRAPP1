a55fe3ad292a7b3ce153b8b1ad1f4bdeb71d1c06
Dave <37315071+DRAPP1@users.noreply.github.com>
2018-03-17 17:19:02 -0500

    College Student

--- src/CollegeStudent.java
+++ src/CollegeStudent.java
@@ -1,13 +1,22 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s): dave rapp
+  	Date: 3-17-18
 */
 
 public class CollegeStudent {
 
 	public static void main(String[] args) {
+    // Declare Variables
+		int Year = 4;
+		int Courses = 5;
+		double GPA = 3.6;
+		
+	// Compute
+		
+		
 
+		
 	}
 
 }
